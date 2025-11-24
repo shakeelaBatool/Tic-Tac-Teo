eeceeea1cadb7551d50cdf435d35c4ce418f40ff
Shakeela Batool <shakeelasadpara@gmail.com>
2025-11-24 14:24:38 +0500

    Fix formatting issues in Tac_Toe.py

--- Tac_Toe.py
+++ Tac_Toe.py
@@ -1,5 +1,5 @@
 import random
-start=int(input("Enter 1 to start Tic Tac Toe: "))
+start= int(input("Enter 1 to start Tic Tac Toe: "))
 if start==1:
     computer = ["❌", "⭕"]                                                                                    #| [x o]
     computer_choice = random.choice(computer)                                                                   #| randomly chioce 
@@ -59,6 +59,7 @@ if start==1:
 else:
      print("Invalid")
      
+
 
 
 
