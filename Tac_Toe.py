cfb3f503748b41c2b66094b8f656c0d03c9c378f
Shakeela Batool <shakeelasadpara@gmail.com>
2025-11-30 14:47:48 +0500

    Update Tac_Toe.py

--- Tac_Toe.py
+++ Tac_Toe.py
@@ -1,6 +1,6 @@
 import random
 start= int(input("Enter 1 to start Tic Tac Toe: "))
-if start==1:
+if start== 1:
     computer = ["❌", "⭕"]                                                                                    #| [x o]
     computer_choice = random.choice(computer)                                                                   #| randomly chioce 
     grid = []                                                                                                   #| [ ]
@@ -59,6 +59,7 @@ if start==1:
 else:
      print("Invalid")
      
+
 
 
 
